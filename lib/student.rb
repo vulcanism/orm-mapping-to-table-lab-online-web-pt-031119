98cf696269677c8caaf3346d64065da7e514ab7a
Savannah C <sgcorrell@gmail.com>
2019-05-21 16:58:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,7 +36,7 @@ class Student
           
       DB[:conn].execute(sql, self.name, self.grade)
       
-      @id = 
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
   
   
