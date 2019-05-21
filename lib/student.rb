0a72b6729af7823c7affdbbf70540cdeb28cc1bf
Savannah C <sgcorrell@gmail.com>
2019-05-21 16:52:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,8 +12,8 @@ class Student
     sql = <<-SQL
           CREATE TABLE IF NOT EXISTS students (
               id INTEGER PRIMARY KEY,
-              
-          
+              name TEXT,
+              grade INTEGER
           );
   end
   
